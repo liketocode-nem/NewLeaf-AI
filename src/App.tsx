45b6eb922ccdc08a01d46804d4140bd09b7113b8
Nathan Miller <davidhmiller00@hotmail.com>
2025-10-02 18:21:59 -0400

    nav blur changed

--- src/App.tsx
+++ src/App.tsx
@@ -4,35 +4,38 @@ import RotatingText from "./components/RotatingText";
 import { LayoutGroup, motion } from "motion/react";
 import { ReactLenis } from "lenis/react";
 import type { LenisRef } from "lenis/react";
-import { cancelFrame, frame } from 'motion/react'
-import {useInView} from "motion/react"
+import { cancelFrame, frame } from "motion/react";
+import { useInView } from "motion/react";
 import { useEffect, useRef, useState } from "react";
-import { FaSalesforce,FaHubspot,FaShopify,FaMailchimp,FaSlack } from "react-icons/fa6";
-import { SiN8N,SiGooglecalendar,SiFirebase,SiGmail } from "react-icons/si";
+import {
+  FaSalesforce,
+  FaHubspot,
+  FaShopify,
+  FaMailchimp,
+  FaSlack,
+} from "react-icons/fa6";
+import { SiN8N, SiGooglecalendar, SiFirebase, SiGmail } from "react-icons/si";
 import { FaGoogleDrive } from "react-icons/fa6";
-import LogoLoop from './components/LogoLoop';
+import LogoLoop from "./components/LogoLoop";
 import { IconContext } from "react-icons";
-import { HiLightBulb } from 'react-icons/hi';
-import { LuTextCursor } from 'react-icons/lu';
-import { BsChatLeftFill } from 'react-icons/bs';
-import ScrollReveal from './components/ScrollReveal';
+import { HiLightBulb } from "react-icons/hi";
+import { LuTextCursor } from "react-icons/lu";
+import { BsChatLeftFill } from "react-icons/bs";
+import ScrollReveal from "./components/ScrollReveal";
 import { log } from "console";
 
-
 const techLogos = [
-  { node: <FaSalesforce />, title: "Salesforce", },
-  { node: <FaHubspot />, title: "Hubspot",},
-  { node: <FaShopify />, title: "Shopfiy",},
-  { node: <FaMailchimp />, title: "Mailchimp",},
-  { node: <FaSlack />, title: "Slack",},
-  { node: <SiN8N />, title: "N8N",},
-  { node: <SiGooglecalendar />, title: "GoogleCalender",},
-  { node: <SiFirebase />, title: "Firebase",},
-  { node: <SiGmail />, title: "Gmail",},
-  { node: <FaGoogleDrive />, title: "GoogleDrive  ",},
-
+  { node: <FaSalesforce />, title: "Salesforce" },
+  { node: <FaHubspot />, title: "Hubspot" },
+  { node: <FaShopify />, title: "Shopfiy" },
+  { node: <FaMailchimp />, title: "Mailchimp" },
+  { node: <FaSlack />, title: "Slack" },
+  { node: <SiN8N />, title: "N8N" },
+  { node: <SiGooglecalendar />, title: "GoogleCalender" },
+  { node: <SiFirebase />, title: "Firebase" },
+  { node: <SiGmail />, title: "Gmail" },
+  { node: <FaGoogleDrive />, title: "GoogleDrive  " },
 ];
-
 
 function App() {
   const lenisRef = useRef<LenisRef>(null);
@@ -43,13 +46,13 @@ function App() {
   const aboutRef = useRef(null);
   const contactRef = useRef(null);
 
-  const heroView = useInView(heroRef,{amount:.5});
-  const servicesView = useInView(servicesRef,{amount:.5});
-  const testimonialView = useInView(testimonialRef,{amount:.5});
-  const aboutView = useInView(aboutRef,{amount:.5});
-  const contactView = useInView(contactRef,{amount:.5});
+  const heroView = useInView(heroRef, { amount: 0.5 });
+  const servicesView = useInView(servicesRef, { amount: 0.5 });
+  const testimonialView = useInView(testimonialRef, { amount: 0.5 });
+  const aboutView = useInView(aboutRef, { amount: 0.5 });
+  const contactView = useInView(contactRef, { amount: 0.5 });
 
-  const [activeSection,setActiveSection] = useState<string>("hero")
+  const [activeSection, setActiveSection] = useState<string>("hero");
 
   useEffect(() => {
     function update(data: { timestamp: number }) {
@@ -64,44 +67,79 @@ function App() {
 
   useEffect(() => {
     if (heroView) {
-      setActiveSection("hero")
+      setActiveSection("hero");
+    } else if (servicesView) {
+      setActiveSection("services");
+    } else if (testimonialView) {
+      setActiveSection("testimonial");
+    } else if (aboutView) {
+      setActiveSection("about");
+    } else if (contactView) {
+      setActiveSection("contact");
     }
-    else if (servicesView) {
-      setActiveSection("services")
-    }
-    else if (testimonialView) {
-      setActiveSection("testimonial")
-    }
-    else if (aboutView) {
-      setActiveSection("about")
-    }
-    else if (contactView) {
-      setActiveSection("contact")
-    }
-  
-  }, [heroView,servicesView,testimonialView,aboutView,contactView])
-  
+  }, [heroView, servicesView, testimonialView, aboutView, contactView]);
 
   return (
     <>
-         <nav
-        className={`w-full xl:h-24 fixed top-0 left-0 flex justify-center items-center xl:p-10 z-50 clash`}
-
+      <nav
+        className={`w-full xl:h-24 fixed xl:top-.5 left-0 flex justify-center items-center xl:p-10 z-50 clash`}
       >
-        <div className="py-4 px-5 w-6/12 xl:text-xl text-white flex justify-between items-center font-black bg-white/10 rounded-4xl backdrop-blur-md border border-white/35 shadow-lg">
-          <a href="#hero" className="xl:text-2xl">NewLeaf/AI</a>
+        <div className="py-4 px-5 w-6/12 xl:text-xl text-white flex justify-between items-center font-black bg-white/10 rounded-4xl backdrop-blur-sm border border-white/35 shadow-lg">
+          <a href="#hero" className="xl:text-2xl">
+            NewLeaf/AI
+          </a>
           <div className="flex gap-8 ">
-            <a href="#services" className={`font-normal${activeSection == "services" ? " after:content-[''] relative after:absolute after:left-0 after:bottom-0 after:w-full after:h-[2px] after:primary-bg after:transition-all after:duration-300" : " after:content-[''] relative after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:primary-bg after:transition-all after:duration-300" }`}>Services</a>
-            <a href="#testimonial" className={`font-normal${activeSection == "testimonial" ? " after:content-[''] relative after:absolute after:left-0 after:bottom-0 after:w-full after:h-[2px] after:primary-bg after:transition-all after:duration-300"  : " after:content-[''] relative after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:primary-bg after:transition-all after:duration-300"}`}>Testimonial</a>
-            <a href="#about" className={`font-normal${activeSection == "about" ? " after:content-[''] relative after:absolute after:left-0 after:bottom-0 after:w-full after:h-[2px] after:primary-bg after:transition-all after:duration-300" : " after:content-[''] relative after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:primary-bg after:transition-all after:duration-300" }`}>About</a>
-            <a href="#contact" className={`font-normal${activeSection == "contact" ? " after:content-[''] relative after:absolute after:left-0 after:bottom-0 after:w-full after:h-[2px] after:primary-bg after:transition-all after:duration-300" : " after:content-[''] relative after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:primary-bg after:transition-all after:duration-300" }`}>Contact</a>
+            <a
+              href="#services"
+              className={`font-normal ${
+                activeSection == "services"
+                  ? " after:content-[''] relative after:absolute after:left-0 after:bottom-0 after:w-full after:h-[2px] after:primary-bg after:transition-all after:duration-300"
+                  : " after:content-[''] relative after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:primary-bg after:transition-all after:duration-300"
+              }`}
+            >
+              Services
+            </a>
+            <a
+              href="#testimonial"
+              className={`font-normal ${
+                activeSection == "testimonial"
+                  ? " after:content-[''] relative after:absolute after:left-0 after:bottom-0 after:w-full after:h-[2px] after:primary-bg after:transition-all after:duration-300"
+                  : " after:content-[''] relative after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:primary-bg after:transition-all after:duration-300"
+              }`}
+            >
+              Testimonial
+            </a>
+            <a
+              href="#about"
+              className={`font-normal ${
+                activeSection == "about"
+                  ? " after:content-[''] relative after:absolute after:left-0 after:bottom-0 after:w-full after:h-[2px] after:primary-bg after:transition-all after:duration-300"
+                  : " after:content-[''] relative after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:primary-bg after:transition-all after:duration-300"
+              }`}
+            >
+              About
+            </a>
+            <a
+              href="#contact"
+              className={`font-normal ${
+                activeSection == "contact"
+                  ? " after:content-[''] relative after:absolute after:left-0 after:bottom-0 after:w-full after:h-[2px] after:primary-bg after:transition-all after:duration-300"
+                  : " after:content-[''] relative after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:primary-bg after:transition-all after:duration-300"
+              }`}
+            >
+              Contact
+            </a>
           </div>
         </div>
       </nav>
 
       <ReactLenis root options={{ autoRaf: false }} ref={lenisRef} />
 
-      <div className="w-full h-screen bg-zinc-950 relative" ref={heroRef} id="hero">
+      <div
+        className="w-full h-screen bg-zinc-950 relative"
+        ref={heroRef}
+        id="hero"
+      >
         <Aurora
           colorStops={["#8dfcbb", "#2be256", "#8dfcf3"]}
           blend={1}
@@ -139,26 +177,30 @@ function App() {
           </div>
         </LayoutGroup>
       </div>
-      <div className="w-full flex justify-center xl:p-8" ref={servicesRef} id="services">
+      <div
+        className="w-full flex justify-center xl:p-8"
+        ref={servicesRef}
+        id="services"
+      >
         <div className="bg-zinc-950 text-white poppins xl:h-120 xl:flex xl:justify-center xl:gap-16 ">
-            <div className="xl:w-150 flex flex-col xl:justify-center ">
-              <div>
-                <h2 className="xl:text-7xl xl:mb-6 underline primary-underline font-medium clash ">
-                  Services
-                </h2>
-                <p className="text-white font-regular xl:text-lg/tight">
-                  NewLeafAI creates AI-powered automation that aim to eliminate time
-                  consuming, annoying, and expensive tasks so you can stop focusing
-                  on the small stuff and get back to what matters most - growing
-                  your business. <br />
-                  <br />
-                  From creating a specialized AI model that can help costumers via
-                  email and a website chat bot, to automating your outreach and
-                  follow-up emails, NewLeafAI creates custom solutions that fit your
-                  specific needs.
-                </p>
-              </div>
+          <div className="xl:w-150 flex flex-col xl:justify-center ">
+            <div>
+              <h2 className="xl:text-7xl xl:mb-6 underline primary-underline font-medium clash ">
+                Services
+              </h2>
+              <p className="text-white font-regular xl:text-lg/tight">
+                NewLeafAI creates AI-powered automation that aim to eliminate
+                time consuming, annoying, and expensive tasks so you can stop
+                focusing on the small stuff and get back to what matters most -
+                growing your business. <br />
+                <br />
+                From creating a specialized AI model that can help costumers via
+                email and a website chat bot, to automating your outreach and
+                follow-up emails, NewLeafAI creates custom solutions that fit
+                your specific needs.
+              </p>
             </div>
+          </div>
           <div className="xl:w-150 h-full xl:flex xl:justify-start xl:items-center">
             <div className="xl:w-full xl:h-full  xl:grid xl:grid-cols-2 xl:grid-rows-2   xl:gap-4">
               <div className="border border-zinc-800 rounded-3xl w-full h-full bento"></div>
@@ -169,33 +211,43 @@ function App() {
           </div>
         </div>
       </div>
-      <div style={{ height: '200px', position: 'relative', overflow: 'hidden'}} className="xl:mt-48">
-        <IconContext.Provider value={{ color: "white"}}>
-
-      <LogoLoop
-        logos={techLogos}
-        speed={60}
-        direction="left"
-        logoHeight={80}
-        gap={70}
-        pauseOnHover
-        ariaLabel="Possible integrations"
-      />
-      </IconContext.Provider>
-
-    </div>
+      <div
+        style={{ height: "200px", position: "relative", overflow: "hidden" }}
+        className="xl:mt-48"
+      >
+        <IconContext.Provider value={{ color: "white" }}>
+          <LogoLoop
+            logos={techLogos}
+            speed={60}
+            direction="left"
+            logoHeight={80}
+            gap={70}
+            pauseOnHover
+            ariaLabel="Possible integrations"
+          />
+        </IconContext.Provider>
+      </div>
       <ScrollReveal
         baseOpacity={0}
         enableBlur={true}
-        baseRotation={2}
+        baseRotation={3}
         blurStrength={70}
-        containerClassName="text-white m-auto w-300 h-200" 
+        containerClassName="text-white m-auto w-300 h-200"
       >
-          We integrate with <span className="primary-underline underline">whatever platforms you use</span>. Whether you are a business who wants automations
-          with salesforce or hubspot, slack or gmail, or any other software not listed, <span className="primary-underline underline">we have you covered</span>. 
-
+        We integrate with{" "}
+        <span className="primary-underline underline">
+          whatever platforms you use
+        </span>
+        . Whether you are a business who wants automations with salesforce or
+        hubspot, slack or gmail, or any other software not listed,{" "}
+        <span className="primary-underline underline">we have you covered</span>
+        .
       </ScrollReveal>
-      <section className="h-screen w-screen" ref={testimonialRef} id="testimonial"></section>
+      <section
+        className="h-screen w-screen"
+        ref={testimonialRef}
+        id="testimonial"
+      ></section>
     </>
   );
 }
